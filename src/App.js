c567b0ad0aaf9e72e96ae341f5952d889006f230
yeasin71 <fixcode800@gmail.com>
2022-11-18 01:55:08 +0600

    Application initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/style/app.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nutrition from './pages/Nutrition/Nutrition';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Nutrition />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Nutrition/NutritionComponent/NutritionComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NutritionComponent() {
+    return (
+        <div>NutritionComponent</div>
+    )
+}
